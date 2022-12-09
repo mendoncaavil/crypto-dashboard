62fea610f4b45ce9e340a1ccd42c91e074e466af
Avil Mendonca <106405181+mendoncaavil@users.noreply.github.com>
2022-12-09 19:15:49 +0530

    get price function

--- src/Pages/Compare.js
+++ src/Pages/Compare.js
@@ -7,18 +7,62 @@ import Loading from "../Components/Common/Loading/Loading";
 import { DASHBOARD_API_URL } from "../Constants";
 import { getCoinData } from "../Functions/GetCoinData";
 import List from "../Components/Dashboard/ListComponent/List";
-import Info from '../Components/Coin/Info/Info'
+import Info from "../Components/Coin/Info/Info";
+import { getCoinPrices } from "../Functions/GetCoinPrices";
+import LineChart from "../Components/Coin/Chart";
+import { getDate } from "../Functions/GetDate";
 
 function ComparePage() {
   const [coin1, setCoin1] = useState("bitcoin");
   const [coin2, setCoin2] = useState("ethereum");
   const [days, setDays] = useState(120);
+  const [priceType, setPriceType] = useState("prices");
 
   const [allCoins, setAllCoins] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const [coinData1, setCoinData1] = useState({});
   const [coinData2, setCoinData2] = useState({});
+
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [],
+        fill: false,
+        borderColor: "#fff",
+        tension: 0.1,
+      },
+    ],
+  });
+
+  const options = {
+    responsive: true,
+
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+
+    scales: {
+      y: {
+        type: "linear",
+        display: true,
+        position: "left",
+      },
+      y1: {
+        type: "linear",
+        display: true,
+        position: "right",
+
+        // grid line settings
+        grid: {
+          drawOnChartArea: false, // only want the grid lines for one axis to show up
+        },
+      },
+    },
+  };
 
   useEffect(() => {
     axios
@@ -39,7 +83,6 @@ function ComparePage() {
     const data2 = await getCoinData(coin2);
 
     if (data1) {
-      console.log("data1 >>>", data1);
       setCoinData1({
         id: data1.id,
         name: data1.name,
@@ -55,7 +98,6 @@ function ComparePage() {
     }
 
     if (data2) {
-      console.log("data2 >>>", data2);
       setCoinData2({
         id: data2.id,
         name: data2.name,
@@ -69,46 +111,85 @@ function ComparePage() {
         market_cap: data2.market_data.market_cap.usd,
       });
     }
+
+    getPrices(coin1, coin2, days, priceType);
+
     setLoading(false);
   };
 
-  const handleCoinChange1 = async (e) => {
-    setCoin1(e.target.value);
-    const data1 = await getCoinData(e.target.value);
-    if (data1) {
-      console.log("data1 >>>", data1);
-      setCoinData1({
-        id: data1.id,
-        name: data1.name,
-        symbol: data1.symbol,
-        image: data1.image.large,
-        desc: data1.description.en,
-        price_change_percentage_24h:
-          data1.market_data.price_change_percentage_24h,
-        total_volume: data1.market_data.total_volume.usd,
-        current_price: data1.market_data.current_price.usd,
-        market_cap: data1.market_data.market_cap.usd,
-      });
-    }
+  const getPrices = async (coin1, coin2, days, priceType) => {
+    const prices1 = await getCoinPrices(coin1, days, priceType);
+    const prices2 = await getCoinPrices(coin2, days, priceType);
+
+    setChartData({
+      labels: prices1?.map((data) => getDate(data[0])),
+      datasets: [
+        {
+          label: `${coin1}`,
+          data: prices1?.map((data) => data[1]),
+          borderWidth: 1,
+          fill: false,
+          tension: 0.25,
+          backgroundColor: "transparent",
+          borderColor: "#3a80e9",
+          pointRadius: 0,
+          yAxisID: "y",
+        },
+        {
+          label: `${coin2}`,
+          data: prices2?.map((data) => data[1]),
+          borderWidth: 1,
+          fill: false,
+          tension: 0.25,
+          backgroundColor: "transparent",
+          borderColor: "#15ff00",
+          pointRadius: 0,
+          yAxisID: "y1",
+        },
+      ],
+    });
   };
 
-  const handleCoinChange2 = async (e) => {
-    setCoin2(e.target.value);
-    const data2 = await getCoinData(e.target.value);
-    if (data2) {
-      console.log("data2 >>>", data2);
-      setCoinData2({
-        id: data2.id,
-        name: data2.name,
-        symbol: data2.symbol,
-        image: data2.image.large,
-        desc: data2.description.en,
-        price_change_percentage_24h:
-          data2.market_data.price_change_percentage_24h,
-        total_volume: data2.market_data.total_volume.usd,
-        current_price: data2.market_data.current_price.usd,
-        market_cap: data2.market_data.market_cap.usd,
-      });
+  const handleCoinChange = async (e, isCoin2) => {
+    if (!isCoin2) {
+      setCoin1(e.target.value);
+      const data1 = await getCoinData(e.target.value);
+
+      if (data1) {
+        setCoinData1({
+          id: data1.id,
+          name: data1.name,
+          symbol: data1.symbol,
+          image: data1.image.large,
+          desc: data1.description.en,
+          price_change_percentage_24h:
+            data1.market_data.price_change_percentage_24h,
+          total_volume: data1.market_data.total_volume.usd,
+          current_price: data1.market_data.current_price.usd,
+          market_cap: data1.market_data.market_cap.usd,
+        });
+
+        getPrices(e.target.value, coin2, days, priceType);
+      }
+    } else {
+      setCoin2(e.target.value);
+      const data2 = await getCoinData(e.target.value);
+
+      if (data2) {
+        setCoinData2({
+          id: data2.id,
+          name: data2.name,
+          symbol: data2.symbol,
+          image: data2.image.large,
+          desc: data2.description.en,
+          price_change_percentage_24h:
+            data2.market_data.price_change_percentage_24h,
+          total_volume: data2.market_data.total_volume.usd,
+          current_price: data2.market_data.current_price.usd,
+          market_cap: data2.market_data.market_cap.usd,
+        });
+        getPrices(coin1, e.target.value, days, priceType);
+      }
     }
   };
 
@@ -120,21 +201,24 @@ function ComparePage() {
           <p>Crypto 1</p>
           <SelectCoin
             coin={coin1}
-            handleChange={(e) => handleCoinChange1(e)}
+            handleChange={(e) => handleCoinChange(e)}
             allCoins={allCoins.filter((coin) => coin.id != coin2)}
           />
 
           <p>Crypto 2</p>
           <SelectCoin
             coin={coin2}
-            handleChange={(e) => handleCoinChange2(e)}
+            handleChange={(e) => handleCoinChange(e, true)}
             allCoins={allCoins.filter((coin) => coin.id != coin1)}
           />
         </div>
         <SelectDays
           noText={true}
           days={days}
-          handleChange={(e) => setDays(e.target.value)}
+          handleChange={(e) => {
+            getPrices(coin1, coin2, e.target.value, priceType);
+            setDays(e.target.value);
+          }}
         />
       </div>
       {loading ? (
@@ -147,6 +231,7 @@ function ComparePage() {
           <div className="grey-container">
             <List coin={coinData2} />
           </div>
+          <LineChart chartData={chartData} options={options} />
           <div className="grey-container">
             <Info name={coinData1.name} desc={coinData1.desc} />
           </div>
